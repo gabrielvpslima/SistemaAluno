a1f82bed24c5d2c210f56e9cd7c27e119d754d66
Gabriel Lima <93952045+gabrielvpslima@users.noreply.github.com>
2021-12-09 20:12:20 -0300

    Update sistemas.js

--- sistemas.js
+++ sistemas.js
@@ -6,22 +6,22 @@ for (let nome of nomeDeAluno) {
 
 
 
-// let numeroDeAlunos = 10;
+let numeroDeAlunos = 10;
 
-// let contador = 0;
+let contador = 0;
 
-// while( contador <= numeroDeAlunos) {
-//     //console.log(contador)
+while( contador <= numeroDeAlunos) {
+     //console.log(contador)
 
-//     if(contador == 0){
-//         console.log("O número atual é zero")
-//     }else if(contador % 2 != 0){
-//         console.log("O número " + contador + " é impar")
-//     }else{
-//         console.log(`O número ${contador} é par`)
-//     }
-//     contador++;       
-// }
+     if(contador == 0){
+         console.log("O número atual é zero")
+     }else if(contador % 2 != 0){
+         console.log("O número " + contador + " é impar")
+     }else{
+         console.log(`O número ${contador} é par`)
+     }
+     contador++;       
+ }
 
 
 
